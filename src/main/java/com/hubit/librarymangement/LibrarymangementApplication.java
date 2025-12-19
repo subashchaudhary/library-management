e45b38999bbed0883a869e013756852bfad48e23
subashtharu <subash.tharu@ismt.edu.np>
2025-12-19 12:14:33 +0545

    spring boot application setup

---
+++ src/main/java/com/hubit/librarymangement/LibrarymangementApplication.java
@@ -0,0 +1,14 @@
+package com.hubit.librarymangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarymangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarymangementApplication.class, args);
+        System.out.println("Application Started...");
+	}
+//http://localhost:8080/
+}
---
+++ src/test/java/com/hubit/librarymangement/LibrarymangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubit.librarymangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarymangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
